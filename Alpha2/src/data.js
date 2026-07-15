041b6835e028a4435be6b52a4a756b60a259cf1e
NguyenLoc231 <hoctaploc231@gmail.com>
2026-07-15 12:05:10 +0700

    rename all class and id to BEM, use html template and change js function to DOM delegration

--- Alpha2/src/data.js
+++ Alpha2/src/data.js
@@ -7,5 +7,6 @@ export class Dataset {
     #notify;
 
 
+    
 }
 
--- Alpha2/src/schedule.js
+++ Alpha2/src/schedule.js
@@ -1,181 +1,103 @@
-const scheduleRowContainer = document.getElementById('schedule-row-container');
-
-let rowCounter = 0;
-
-function createScheduleRowColorDisplay(){
-  const scheduleRowColorDisplay = document.createElement('button');
-  scheduleRowColorDisplay.type = 'button';
-  scheduleRowColorDisplay.classList.add('schedule-row__color-display');
-  scheduleRowColorDisplay.addEventListener('click', openScheduleRowColorDropdown);
-
-  return scheduleRowColorDisplay;
-}
-
-function createScheduleRowColorDropdown(){
-  const scheduleRowColorDropdown = document.createElement('div');
-  scheduleRowColorDropdown.classList.add('schedule-row__color-dropdown');
-  const rowColor = [{name: 'WHITE', color: '#FAFAFA'}, {name: 'YELLOW', color: '#FFD700'}, {name: 'RED', color: '#FF1744'}];
-  for (const item of rowColor){
-    const scheduleRowColorButton = document.createElement('button');
-    scheduleRowColorButton.type = 'button';
-    scheduleRowColorButton.dataset.color = item.color;
-    scheduleRowColorButton.textContent = item.name;
-    scheduleRowColorButton.addEventListener('click', takeScheduleColor);
-    scheduleRowColorDropdown.appendChild(scheduleRowColorButton);
-  }
-
-  return scheduleRowColorDropdown;
-}
-
-function openScheduleRowColorDropdown(event){
-  const colorDropdown = event.target.closest('.schedule-row__color').querySelector('.schedule-row__color-dropdown');
-  colorDropdown.classList.toggle('schedule-row__color-dropdown--open');
-}
-
-function takeScheduleColor(event){
-  const scheduleRowColorDisplay = event.target.closest('.schedule-row__color').querySelector('.schedule-row__color-display')
-  const color = event.target.dataset.color;
-  scheduleRowColorDisplay.style.backgroundColor = color;
-}
-
-function createScheduleRowColor(){
-  const scheduleRowColor = document.createElement('div');
-  scheduleRowColor.classList.add('schedule-row__color');
-  scheduleRowColor.appendChild(createScheduleRowColorDisplay());
-  scheduleRowColor.appendChild(createScheduleRowColorDropdown());
-
-  return scheduleRowColor;
-}
-
-function createScheduleRowContent(){
-  const newRowContent = document.createElement('input');
-  newRowContent.classList.add('schedule-row__content');
-  newRowContent.setAttribute('type', 'text');
-  newRowContent.placeholder = 'Nhập nội dung';
-  return newRowContent;
-}
-
-function createScheduleRowTime(){
-  const newScheduleRowTime = document.createElement('input');
-  newScheduleRowTime.classList.add('schedule-row__time');
-  newScheduleRowTime.type = 'text';
-  newScheduleRowTime.placeholder = '10h37';
-  newScheduleRowTime.addEventListener('focus', function() {
-    this.type='time';
-    this.classList.add('schedule-row__time-onfocus');
-  });
-  newScheduleRowTime.addEventListener('blur', function() {
-    this.type='text';
-    this.classList.remove('schedule-row__time-onfocus');
-  });
-  return newScheduleRowTime;
-}
-
-function createScheduleRowDate(){
-  const newScheduleRowDate = document.createElement('input');
-  newScheduleRowDate.classList.add('schedule-row__date');
-  newScheduleRowDate.type = 'text';
-  newScheduleRowDate.placeholder = '10/07/2026';
-  newScheduleRowDate.addEventListener('focus', function() {
-    this.type='date';
-    this.classList.add('schedule-row__date-onfocus');
-  });
-  newScheduleRowDate.addEventListener('blur', function() {
-    this.type='text';
-    this.classList.remove('schedule-row__date-onfocus');
-  });
-  return newScheduleRowDate;
-}
-
-function createScheduleRowNotify(){
-  const newScheduleRowNotify = document.createElement('button');
-  newScheduleRowNotify.classList.add('schedule-row__notify');
-  newScheduleRowNotify.type = 'button';
-  newScheduleRowNotify.textContent = 'O';
-  return newScheduleRowNotify;
-}
-
-function scheduleRowDelete(event){
-  event.target.closest('.schedule-row').remove();
-}
-
-function createScheduleRowDelete(){
-  const newScheduleRowDelete = document.createElement('button');
-  newScheduleRowDelete.classList.add('schedule-row__delete');
-  newScheduleRowDelete.type = 'button';
-  newScheduleRowDelete.innerText = `X`;
-  newScheduleRowDelete.addEventListener('click', scheduleRowDelete)
-  return newScheduleRowDelete;
-}
-
-function createScheduleRowMenuButton(){
-  const newScheduleRowMenuButton = document.createElement('button');
-  newScheduleRowMenuButton.classList.add('schedule-row__menu-button');
-  newScheduleRowMenuButton.type = 'button';
-  newScheduleRowMenuButton.innerHTML = `O<br>O<br>O`;
-  newScheduleRowMenuButton.addEventListener('click', openScheduleRowMenuDropdown);
-  return newScheduleRowMenuButton;
-}
-
-function createScheduleRowMenuDropdown(){
-  const newScheduleRowMenuDropdown = document.createElement('div');
-  newScheduleRowMenuDropdown.classList.add('schedule-row__menu-dropdown');
-  newScheduleRowMenuDropdown.appendChild(createScheduleRowMenuDropdownContainer());
-  return newScheduleRowMenuDropdown;
-}
-
-function openScheduleRowMenuDropdown(event){
-  const menuDropdown = event.target.closest('.schedule-row__menu').querySelector('.schedule-row__menu-dropdown');
-  menuDropdown.classList.toggle('schedule-row__menu-dropdown--open');
-}
-
-function createScheduleRowMenuDropdownContainer(){
-  const newScheduleRowMenuDropdownContainer = document.createElement('div');
-  newScheduleRowMenuDropdownContainer.classList.add('schedule-row__menu-dropdown-container');
-  newScheduleRowMenuDropdownContainer.appendChild(createScheduleRowColor());
-  newScheduleRowMenuDropdownContainer.appendChild(createScheduleRowNotify());
-  newScheduleRowMenuDropdownContainer.appendChild(createScheduleRowDelete());
-  return newScheduleRowMenuDropdownContainer;
-}
-
-function createScheduleRowMenu(){
-  const newScheduleRowMenu = document.createElement('div');
-  newScheduleRowMenu.classList.add('schedule-row__menu');
-  newScheduleRowMenu.appendChild(createScheduleRowMenuButton());
-  newScheduleRowMenu.appendChild(createScheduleRowMenuDropdown());
-  return newScheduleRowMenu;
-}
-
 function createScheduleRow(){
-  rowCounter++;
+  const template = document.getElementById('schedule__row--template');
+  const newRow = template.content.cloneNode(true);
 
-  const newScheduleRow = document.createElement('div');
-  newScheduleRow.classList.add('schedule-row');
+  document.getElementById('list__container').appendChild(newRow);
 
-  newScheduleRow.appendChild(createScheduleRowTime());
-  newScheduleRow.appendChild(createScheduleRowContent());
-  newScheduleRow.appendChild(createScheduleRowDate());
-  newScheduleRow.appendChild(createScheduleRowMenu());
-
-  scheduleRowContainer.appendChild(newScheduleRow);
-
-  console.log('created schedule ' + rowCounter)
 }
+
+// ===== focus in =====
+
+function focusin(event){
+  if(event.target.classList.contains('row__time')){
+    event.target.type = 'time';
+    event.target.classList.add('row__time--onfocus');
+  }
+  if(event.target.classList.contains('row__date')){
+    event.target.type = 'date';
+    event.target.classList.add('row__date--onfocus');
+  }  
+}
+
+// ===== focus out =====
+
+function focusout(event){
+  if(event.target.classList.contains('row__time')){
+    event.target.type = 'text';
+    event.target.classList.remove('row__time--onfocus');
+  }
+  if(event.target.classList.contains('row__date')){
+    event.target.type = 'text';
+    event.target.classList.remove('row__date--onfocus');
+  }
+}
+
+// ===== menu dropdown =====
+
+function openMenuDropdown(event){
+  if(event.target.classList.contains('menu__button')){
+    event.target.closest('.row__menu').querySelector('.menu__dropdown').classList.toggle('menu__dropdown--open');
+  }
+}
+
+// ===== color dropdown =====
+
+function openColorDropdown(event){
+  if(event.target.classList.contains('color__button')){
+    event.target.closest('.row__color').querySelector('.color__dropdown').classList.toggle('color__dropdown--open')
+  }
+}
+
+// ===== color taker =====
+
+function colorTaker(event){
+  if(event.target.classList.contains('color__taker')){
+    event.target.closest('.row__color').querySelector('.color__button')
+    .style.backgroundColor = event.target.dataset.color;
+  }
+}
+
+// ===== row delete =====
+
+function deleteRow(event){
+  if(event.target.classList.contains('row__delete')){
+    event.target.closest('.schedule__row').remove();
+  }
+}
+
+// ===== list click group ====
+
+function handleListClick(event){
+  openMenuDropdown(event);
+  openColorDropdown(event);
+  deleteRow(event);
+  colorTaker(event);
+}
+
+// ===== schedule list =====
+
+const listContainer = document.getElementById('list__container');
+listContainer.addEventListener('click', handleListClick);
+listContainer.addEventListener('focusin', focusin);
+listContainer.addEventListener('focusout', focusout);
+
+// ===== add row =====
 
 const addNewScheduleRow = document.getElementById('add-schedule-row');
 addNewScheduleRow.addEventListener('click', createScheduleRow);
 
-document.addEventListener('click', function(event) {
-    // Nếu click KHÔNG nằm trong bất kỳ color nào → đóng tất cả dropdown
-    if (!event.target.closest('.schedule-row__color')) {
-      document.querySelectorAll('.schedule-row__color-dropdown--open')
-        .forEach(dropdown => dropdown.classList.remove('schedule-row__color-dropdown--open'));
-    }
+// ===== document event =====
 
-    if (!event.target.closest('.schedule-row__menu')) {
-      document.querySelectorAll('.schedule-row__menu-dropdown--open')
-        .forEach(dropdown => dropdown.classList.remove('schedule-row__menu-dropdown--open'));
-    }
-});
+document.addEventListener('click', function(event){
+  if(!event.target.closest('.row__menu')){
+    document.querySelectorAll('.menu__dropdown')
+    .forEach(dropdown => dropdown.classList.remove('menu__dropdown--open'));
+  }
 
-export {createScheduleRow, addNewScheduleRow};
+  if(!event.target.closest('.row__color')){
+    document.querySelectorAll('.color__dropdown')
+    .forEach(dropdown => dropdown.classList.remove('color__dropdown--open'));
+  }
+})
+
+
